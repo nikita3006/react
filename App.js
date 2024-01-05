f108a9e6ef49402e21f5e65d6600598e4f201052
nikita3006 <nikitanegi3011@gmail.com>
2024-01-05 22:39:47 +0530

    implementing  react18 code

--- App.js
+++ App.js
@@ -1,4 +1,9 @@
-const heading = React.createElement("h1", {id:"heading"}, "REACT");
+const heading = React.createElement("div", {id:"parent"},
+ [React.createElement("div", {id:"child 1"},
+[ React.createElement("h1",{},"I am h1 tag"),  React.createElement("h2",{},"I am h2 tag")])],
+React.createElement("div", {id:"child 2"},
+[ React.createElement("h1",{},"I am h1 tag"),  React.createElement("h2",{},"I am h2 tag")]));
+
 console.log(heading) //this is nothing but just a react h1 element and react element is nothing but just a javascript object
  
 const Root = ReactDOM.createRoot(document.getElementById("root"));
