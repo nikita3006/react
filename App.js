cb3e51e86c6602baa272182995307e0c5d2270a4
nikita3006 <nikitanegi3011@gmail.com>
2024-01-10 22:21:28 +0530

    create react element

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+
+//react element
+
+const heading = React.createElement("h2",{},"Niikuu");
+console.log(heading)
+const nik = ReactDOM.createRoot(document.getElementById("root"));
+nik.render(heading)
 
 
-
-const heading = React.createElement("div", {id:"parent"},
- [React.createElement("div", {id:"child 1"},
-[ React.createElement("h1",{},"I am h1 tag"),  React.createElement("h2",{},"I am h2 tag")])],
-React.createElement("div", {id:"child 2"},
-[ React.createElement("h1",{},"I am h1 tag"),  React.createElement("h2",{},"I am h2 tag")]));
-
-console.log(heading) //this is nothing but just a react h1 element and react element is nothing but just a javascript object
- 
-const Root = ReactDOM.createRoot(document.getElementById("root"));
-Root.render(heading) //render job - convert javascript obj and create h1 tag then render into root
