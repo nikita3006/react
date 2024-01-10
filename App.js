11c6e8f95c680ef904707c2b55c950c2348abbae
nikita3006 <nikitanegi3011@gmail.com>
2024-01-10 21:47:46 +0530

    update readme file remove cdn from index.html

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
+
+
 const heading = React.createElement("div", {id:"parent"},
  [React.createElement("div", {id:"child 1"},
 [ React.createElement("h1",{},"I am h1 tag"),  React.createElement("h2",{},"I am h2 tag")])],
