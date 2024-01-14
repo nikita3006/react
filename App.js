a49fc2075e3aa2e37d7ada25cf11b095863e05cc
nikita3006 <nikitanegi3011@gmail.com>
2024-01-15 00:19:12 +0530

    updating README file

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-//react element
+// JSX is like HTML or XML syntax
+// const jsxHeading = <h1 className='head'>Nikita writing using jsx</h1>;
+// console.log(jsxHeading); // Remove the second argument "red"
 
-const heading = React.createElement("h2",{},"Niikuu");
-console.log(heading)
-const nik = ReactDOM.createRoot(document.getElementById("root"));
-nik.render(heading)
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
+
+const elm = <span>nikita react element</span>
+const Title = ()=>(
+    <h1>  
+        Hi I am react 
+    </h1>
+)
+    
 
 
+//React functional Component
+const Header = () => {
+  return (
+    <>
+      {/* <h2>{20000}</h2> */}
+      <h1>{elm}</h1>
+      {Title()}
+      <Title/>
+      <Title></Title>
+      <h1>Functional Component</h1>
+    </>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header />);
