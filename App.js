9a2f288636284423f353ff2f72c59455399c56b0
nikita3006 <nikitanegi3011@gmail.com>
2024-01-16 00:26:57 +0530

    passing props as a argument

--- App.js
+++ App.js
@@ -4,9 +4,10 @@ import  ReactDOM  from 'react-dom/client';
 import logo from './images/logo..foodie.png';
 import './App.css';
 import './Header.css';
-import './Footer.css'
+import './Footer.css';
+import food from './images/food1.jpg'
                           
-const Header = ()=> {
+const Header = () => {
   return(
      <div className='header'>
         <div className='logo-image'>
@@ -22,7 +23,41 @@ const Header = ()=> {
      </div>
   )
 }
+const ResCard =(props) =>{
+  console.log(props)
+  return(
+    <div className='layout'>
+    <div className='container'>
+      <div className='resCard'>
+          <span className='card-image'>
+            <img src={food} alt='card-image' className='image'/>
+          </span>
+        <div className='card-details'>
+            <h3>{props.title}</h3>
+            <h4>{props.cuisine}</h4>
+            <h5>{props.rating}</h5>
+            <h5>{props.time}</h5>
+        </div>
+      </div>
+    </div>
+    </div>
+    
+  )
+}
+const Body = () => {
+  return(
+    <>
+    <div className='searchLayout'>
+      <input type='search' placeholder='search' className='searchInput'/>
+    </div>
+    <div className='resList'>
+        <ResCard  title="kfc" cuisine="indian" rating="4 stars" time="30 min"/>
+        <ResCard  title="alu" cuisine="indian" rating="3 stars" time="40 min"/> 
+    </div>
+    </>
+  )
 
+}
 
 const Footer = () =>{
   return(
@@ -45,6 +80,7 @@ const AppLayout = () =>{
   return(
    <div className='app'>
       <Header/>
+        <Body/>
       <Footer/>
    </div>
   )
