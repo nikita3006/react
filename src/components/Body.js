008cd2566eec465bc8e4954640eb9888bf6936a5
nikita3006 <nikitanegi3011@gmail.com>
2024-05-22 22:11:56 +0530

    changing api and getting value

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,9 +3,10 @@ import ResCard from "./ResCard";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
-  console.log("render")
+  console.log("render");
   const [listOfRes, setListRes] = useState([]); // Initialize filterRes as an empty array
   const [searchText, setSearchText] = useState(""); //to get the data whatever user search if i want to show as a suggestion i need to bind the data with input value
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
   useEffect(() => {
     fetchData();
@@ -14,13 +15,17 @@ const Body = () => {
   const fetchData = async () => {
     try {
       const response = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.957504937859255&lng=77.71029096096754&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=29.1973442&lng=78.97265879999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       );
       const json = await response.json();
       // console.log(json);
 
       setListRes(
-        json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+      setFilteredRestaurant(
+        json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
           ?.restaurants
       );
       // console.log(restaurantData)
@@ -37,9 +42,8 @@ const Body = () => {
   const handlerSearch = () => {
     //filter the card and update the ui
     //searchText
-   console.log(searchText)
+    console.log(searchText);
   };
-
 
   return listOfRes.length === 0 ? (
     <Shimmer />
@@ -52,9 +56,11 @@ const Body = () => {
             placeholder="search"
             value={searchText}
             onChange={(e) => {
-              setSearchText(e.target.value)
-              let filteredRestaurant = listOfRes.filter((data)=> data.info.name.toLowerCase().includes(searchText.toLowerCase()))
-              setListRes(filteredRestaurant)
+              setSearchText(e.target.value);
+              let filteredRestaurants = listOfRes.filter((data) =>
+                data.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filteredRestaurants);
             }}
           />
           <button onClick={handlerSearch}>Search</button>
@@ -66,7 +72,7 @@ const Body = () => {
         </div>
       </div>
       <div className="resList">
-        {listOfRes.map((Data) => (
+        {filteredRestaurant.map((Data) => (
           <ResCard key={Data.id} props={Data} />
         ))}
       </div>
